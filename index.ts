10e8a41704c6b64208b84692d01cf7faae3dae73
Сапелко Сергей Игоревич <sopelmurman@gmail.com>
2025-05-05 10:36:27 +0300

    task 1 result solution

--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
-interface PriceOptions {
+interface TotalPriceOptions {
     price: number;
     discount?: number;
-    isInstallment: boolean;
+    isInstallment?: boolean;
     months?: number;
-}
-
-const totalPrice = ({ price, discount = 0, isInstallment, months = 0  } : PriceOptions) : number => {
-    let res = price;
-    if (discount){
-        res *= (1-(discount / 100));
+  }
+  
+  const totalPrice = ({
+    price,
+    discount = 0,
+    isInstallment,
+    months = 12,
+  }: TotalPriceOptions): number => {
+    if (discount > 1) {
+      discount = discount / 100;
     }
-    if (isInstallment && months){
-        res /= months
-    }
-
-    return res;
-
-
-};
+  
+    const discountedPrice = price - price * discount;
+    const result = isInstallment ? discountedPrice / months: discountedPrice;
+  
+    return Math.round(result);
+  };
+  
+  const total = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 }); // 6250
+  console.log(total);
